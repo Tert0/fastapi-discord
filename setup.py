36bac22cc40b29201b0d29787b462a80e8bfd71b
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-08-16 09:38:35 +0200

    v0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ readme = open("README.md", "r").read()
 setup(
     name="fastapi_discord",
     packages=find_packages(),
-    version="0.1.6",
+    version="0.2.0",
     description="Discord OAuth FastAPI extension for APIs",
     long_description=readme,
     long_description_content_type="text/markdown",
