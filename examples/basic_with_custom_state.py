e10a2307828f8ab106582fe1b1a6ea11d3433ecc
Tert0 <62036464+Tert0@users.noreply.github.com>
2021-08-16 09:36:40 +0200

    Added Example with State Parameter

---
+++ examples/basic_with_custom_state.py
@@ -0,0 +1,64 @@
+from typing import List
+
+from fastapi import Depends, FastAPI
+from fastapi.responses import JSONResponse
+from fastapi_discord import DiscordOAuthClient, RateLimited, Unauthorized, User
+from fastapi_discord.models import GuildPreview
+
+app = FastAPI()
+
+discord = DiscordOAuthClient(
+    "<client-id>", "<client-secret>", "<redirect-url>", ("identify", "guilds", "email")
+)  # scopes
+
+
+@app.get("/login")
+async def login():
+    return {"url": discord.get_oauth_login_url(state="my state")}
+
+
+@app.get("/callback")
+async def callback(code: str, state: str):
+    token, refresh_token = await discord.get_access_token(code)
+    assert state == "my state"
+    return {"access_token": token, "refresh_token": refresh_token}
+
+
+@app.get(
+    "/authenticated",
+    dependencies=[Depends(discord.requires_authorization)],
+    response_model=bool,
+)
+async def isAuthenticated(token: str = Depends(discord.get_token)):
+    try:
+        auth = await discord.isAuthenticated(token)
+        return auth
+    except Unauthorized:
+        return False
+
+
+@app.exception_handler(Unauthorized)
+async def unauthorized_error_handler(_, __):
+    return JSONResponse({"error": "Unauthorized"}, status_code=401)
+
+
+@app.exception_handler(RateLimited)
+async def rate_limit_error_handler(_, e: RateLimited):
+    return JSONResponse(
+        {"error": "RateLimited", "retry": e.retry_after, "message": e.message},
+        status_code=429,
+    )
+
+
+@app.get("/user", dependencies=[Depends(discord.requires_authorization)], response_model=User)
+async def get_user(user: User = Depends(discord.user)):
+    return user
+
+
+@app.get(
+    "/guilds",
+    dependencies=[Depends(discord.requires_authorization)],
+    response_model=List[GuildPreview],
+)
+async def get_guilds(guilds: List = Depends(discord.guilds)):
+    return guilds
